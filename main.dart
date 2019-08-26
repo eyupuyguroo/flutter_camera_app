378c83b060d248d80223c8a196df5cedd600b0de
FE Studio <54513109+eyupuyguroo@users.noreply.github.com>
2019-08-26 13:31:18 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        //body :burası önemli,
+      ),
+    );
+  }
+}
+
+ 
+
+
